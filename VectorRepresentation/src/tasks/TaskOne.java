f7eb9bb106339272917663f64f677b3fb01fa9dd
PeterJeng <peterjeng1@gmail.com>
2018-04-18 01:50:26 -0400

    Added normalization to vectors

--- VectorRepresentation/src/tasks/TaskOne.java
+++ VectorRepresentation/src/tasks/TaskOne.java
@@ -1,9 +1,25 @@
 package tasks;
 
+import java.util.ArrayList;
+
 public class TaskOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<WordNode> test = new ArrayList<WordNode>();
+		ArrayList<Float> alpha = new ArrayList<Float>();
+		ArrayList<Float> beta = new ArrayList<Float>();
+
+		alpha.add(1.0f);
+		alpha.add(4.0f);
+		alpha.add(5.0f);
+
+		WordNode a = new WordNode("test", alpha);
+		beta = a.normalize();
+
+		for (int i = 0; i < beta.size(); i++) {
+			System.out.println(beta.get(i).toString());
+		}
+		
 
 	}
 
